d43b39d032e538148d7422b77bc7155e6cb16cfd
Sanya Baranov <gettofarmila@gmail.com>
2026-06-08 22:08:00 +0300

    feat: Добавлена поддержка socks5/http прокси

--- fpx/main.py
+++ fpx/main.py
@@ -5,7 +5,7 @@ from fpx.classes.runner.runner import Runner
 from fpx.fsm import MemoryStorage, BaseStorage
 
 class FunPayTools:
-    def __init__(self, gkey, storage: BaseStorage | None = None):
+    def __init__(self, gkey, storage: BaseStorage | None = None, proxy = None, http_client = None):
         self._cookies = {
             'golden_key': gkey,
             'locale': 'ru'
@@ -14,15 +14,27 @@ class FunPayTools:
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0",
             "Accept-Language": "ru-RU,ru;q=0.9"
         }
-        self._client = httpx.AsyncClient(
-            http2=True,
-            cookies=self._cookies,
-            headers=self._headers,
-            base_url='https://funpay.com',
-            follow_redirects=True,
-            limits=httpx.Limits(max_keepalive_connections=5, max_connections=10),
-            timeout=httpx.Timeout(15.0)
-        )
+        mounts = {}
+        if proxy:
+            mounts = {
+                "http://": httpx.AsyncHTTPTransport(proxy=proxy),
+                "https://": httpx.AsyncHTTPTransport(proxy=proxy),
+            }
+        if http_client:
+            self._client = http_client
+            self._client.cookies.update(self._cookies)
+            self._client.headers.update(self._headers)
+        else:
+            self._client = httpx.AsyncClient(
+                http2=True,
+                cookies=self._cookies,
+                headers=self._headers,
+                base_url='https://funpay.com',
+                follow_redirects=True,
+                limits=httpx.Limits(max_keepalive_connections=5, max_connections=10),
+                timeout=httpx.Timeout(15.0),
+                mounts=mounts
+            )
         self.account = Account(self._client)
         self.runner = Runner(self.account)
         self.router = self.runner.router
